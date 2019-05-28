69fe2c954b6452f30620d9b88eb74660aeed14bf
tecsup <tecsup@gmail.com>
2019-05-28 16:13:35 -0500

    it added styles

--- features/login/components/login/login.container.js
+++ features/login/components/login/login.container.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
 
+const form = StyleSheet.create({
+  row: {
+    marginBottom: 20
+  },
+  control: {
+    borderColor: '#444',
+    borderWidth: 1
+  }
+})
 class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      password: ''
+    }
+  }
+
+  handleInputChange(stateName, value) {
+    this.setState({
+      [stateName]: value
+    })
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Login</Text>
+      <View>
+        <View>
+          <Text>Login</Text>
+        </View>
+        <View style={form.row}>
+          <Text>Usuario</Text>
+          <TextInput
+            style={form.control}
+            onChangeText={this.handleInputChange.bind(this, 'user')}
+            value={this.state.text}
+          />
+        </View>
+        <View style={form.row}>
+          <Text>Contraseña</Text>
+          <TextInput
+            style={form.control}
+            onChangeText={this.handleInputChange.bind(this, 'password')}
+            value={this.state.text}
+          />
+        </View>
       </View>
     );
   }
